16d0181ca9081d578f742ea63687c8ff006c42db
Daniel Tan <danieltan.singapore@gmail.com>
2025-01-08 09:51:36 +0800

    Update

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,11 @@
 FROM python:3.12-slim-bookworm
 
-WORKDIR /usr/src/app
+WORKDIR /app
 
 COPY requirements.txt .
 RUN pip install -r requirements.txt
 
 COPY . .
 
-EXPOSE 5001
+CMD ["gunicorn", "--bind", "0.0.0.0:$PORT", "app:app"]
 
-CMD ["python", "app.py"]
--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def hello_world():
+def hello():
     return "There was talk of gerbils"
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5001)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
